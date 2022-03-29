7c8a04f9c24a1fd0b038149797ac811299ec5cf8
RoryMack <99274597+RoryMack2@users.noreply.github.com>
2022-03-29 20:30:34 +0000

    added js

---
+++ js/style.js
@@ -0,0 +1,27 @@
+// Copyright Rory Mackay
+// Created by: Rory Mackay
+// Date: March 2022
+// This file contains the JS functions for the index.html file
+
+"use strict"
+
+// declare constants
+const TAX_BRACKET = 0.1805
+
+/**
+ * This function calculates area and perimeter of rectangle.
+ */
+function payClicked () {
+  // get user input
+  let hours = parseFloat(document.getElementById('hours').value)
+  let rate = parseFloat(document.getElementById('rate').value)
+
+  // calculate the pay and the tax
+  let total_pay = hours * rate
+  let tax = TAX_BRACKET * total_pay
+	let total_earned = total_pay - tax 
+
+  // display the results
+  document.getElementById('total-earned').innerHTML = "Your pay will be $" + total_earned.toFixed(2)
+  document.getElementById('tax').innerHTML = "The government will take $" + tax.toFixed(2)
+}
